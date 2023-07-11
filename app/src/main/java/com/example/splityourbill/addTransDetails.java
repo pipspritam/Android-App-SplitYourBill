ae2bf61cc452b283686263ea1b4ae8393fb520a4
Pritam Mondal <pritammondalpips@gmail.com>
2023-07-11 16:32:18 +0530

    create transaction page, create button visibility set

---
+++ app/src/main/java/com/example/splityourbill/addTransDetails.java
@@ -0,0 +1,17 @@
+package com.example.splityourbill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class addTransDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trans_details);
+
+
+    }
+}

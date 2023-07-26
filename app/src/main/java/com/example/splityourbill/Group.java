7e6e1b73157aab47491e4b3453560eb24f7e924c
Pritam Mondal <pritammondalpips@gmail.com>
2023-07-27 03:16:55 +0530

    add person feature added safe state

---
+++ app/src/main/java/com/example/splityourbill/Group.java
@@ -0,0 +1,18 @@
+package com.example.splityourbill;
+
+public class Group {
+    String name;
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public String getGroupName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
